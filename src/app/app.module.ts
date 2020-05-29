7be3f222f9bc82d0a1214d38def09d0f1bbc6aa3
Nancy Rubiela Serrato Aldana <nserrato@cundinamarca.gov.co>
2020-05-28 20:53:52 -0500

    aleja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
+import { EditarComponent } from './editar/editar.component';
+import { RegistrarComponent} from './registrar/registrar.component';
+import { EventoComponent } from './evento/evento.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditarComponent,
+    RegistrarComponent,
+    EventoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+    MatFormFieldModule,
+    MatCardModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registrar/registrar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar',
+  templateUrl: './Registrar.component.html',
+  styleUrls: ['./Registrar.component.css']
+})
+export class RegistrarComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+  
+
+}
